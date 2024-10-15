f7e840aef0b1d84be63251e86e7c59b6de48efe4
Denipereira <164106702+Denipereira@users.noreply.github.com>
2024-10-15 05:06:29 -0300

    Estilização da página de Login

--- www/js/routes.js
+++ www/js/routes.js
@@ -30,6 +30,7 @@ var app = new Framework7({
         },
         pageInit: function (event, page) {
           // fazer algo quando a página for inicializada
+          app.views.main.router.navigate('/login/')
           $.getScript('js/index.js');
 
   
@@ -98,12 +99,32 @@ var app = new Framework7({
         },
       }
     },
+    {
+      path: '/login/',
+      url: 'login.html',
+      animate: false,
+      on: {
+        pageBeforeIn: function (event, page) {
+          // fazer algo antes da página ser exibida
+          $("#menuPrincipal").hide("fast");
+        },
+        pageAfterIn: function (event, page) {
+          // fazer algo depois da página ser exibida
+        },
+        pageInit: function (event, page) {
+          // fazer algo quando a página for inicializada
+        },
+        pageBeforeRemove: function (event, page) {
+          // fazer algo antes da página ser removida do DOM
+        },
+      }
+    },
   ],
   // ... other parameters
 });
 
 //Para testes direto no navegador
-var mainView = app.views.create('.view-main', { url: '/link4/' });
+var mainView = app.views.create('.view-main', { url: '/login/' });
 
 //EVENTO PARA SABER O ITEM DO MENU ATUAL
 app.on('routeChange', function (route) {
